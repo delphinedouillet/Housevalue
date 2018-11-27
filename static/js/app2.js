516080e9ba4c0ceae5d9b8395bc388af4b4ea9db
Delphine Albrecht <delphine.albrecht@gmail.com>
2018-11-27 16:57:55 -0600

    c

--- static/js/app2.js
+++ static/js/app2.js
@@ -1,4 +1,4 @@
-Plotly.d3.csv('https://raw.githubusercontent.com/delphinedouillet/Housevalue/delphine/static/js/plotly.csv', function(err, rows){
+Plotly.d3.csv('https://github.com/delphinedouillet/Housevalue/blob/master/static/js/plotly.csv', function(err, rows){
 
     function unpack(rows, key) {
         return rows.map(function(row) { return row[key]; });
