9b1f638ce5a7e7eb10d43d3bf9dcbe221dcf6d55
Delphine Albrecht <delphine.albrecht@gmail.com>
2018-11-28 18:18:26 -0600

    path

--- HousevalueApp/app.py
+++ HousevalueApp/app.py
@@ -10,22 +10,10 @@ import pymongo
 # Create an instance of our Flask app.
 app = Flask(__name__)
 
-# # Create connection variable
-# conn = 'mongodb://localhost:27017'
-
-# # Pass connection to the pymongo instance.
-# client = pymongo.MongoClient(conn)
-
-# # Connect to a database. Will create one if not already available.
-# db = client.map_db
-
 
 # Set route
 @app.route('/')
 def index():
-    # Store the entire team collection in a list
-    # Housing = list(db.housing.find())
-    # print(Housing)
 
     # Return the template with the teams list passed in
     return render_template('index.html')
@@ -46,18 +34,6 @@ def d3():
 def plot():
     # Return the template with the teams list passed in
     return render_template('plots.html')
-
-# @app.route("/Housingdata")
-# def Housingdata():
-#     connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-#     collection = connection[DBS_NAME][COLLECTION_NAME]
-#     projects = collection.find(projection=FIELDS)
-#     json_projects = []
-#     for project in projects:
-#         json_projects.append(project)
-#     json_projects = json.dumps(json_projects, default=json_util.default)
-#     connection.close()
-#     return json_projects
 
 if __name__ == "__main__":
     app.run(debug=True)
